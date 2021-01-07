65ffb14d7b021c4859e09cfec8becd470a3a8efa
Anton Alexandrenok <antonalexandrenok@gmail.com>
2021-01-07 18:52:45 +0300

    fix: Add compatibility with Q.nfcall

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ const getRecommendedBumpOpts = require("./get_recommended_bump_opts.js");
 
 // No async function here because Lerna has buggy detection in resolveConfigPromise().
 module.exports = function getChangelogPreset(config = {}) {
+	let finalizer = result => result;
+
+	if (typeof config === "function") {
+		const cb = config;
+
+		finalizer = result => cb(undefined, result);
+		config = {};
+	} else if (typeof config !== "object") {
+		throw new Error("Invalid config parameter type in preset factory.");
+	}
+
 	const parserOpts = getParserOpts(config);
 	const recommendedBumpOpts = getRecommendedBumpOpts(config);
 
@@ -14,5 +25,5 @@ module.exports = function getChangelogPreset(config = {}) {
 		parserOpts,
 		recommendedBumpOpts,
 		writerOpts,
-	}));
+	})).then(finalizer);
 };
